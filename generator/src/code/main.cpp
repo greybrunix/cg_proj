7c1d558d0a8c795a1550c821eb092e4be88c52ab
Pereirics <joao.lc.pereira1@gmail.com>
2024-02-24 15:44:06 +0000

    Plane formula done

---
+++ generator/src/code/main.cpp
@@ -0,0 +1,133 @@
+#include <GL/gl.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <math.h>
+
+#include <cstdlib>
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+	// Set perspective
+	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+
+void drawPlane(float l, float d) {
+    
+    float x = l/2;
+    float z = -l/2;
+    float diff = l/d;
+    float xc = 0.02;
+    float yc = 0.02;
+    float zc = 0.02;
+
+    glBegin(GL_TRIANGLES);
+    for (int i=0; i<d; i++){
+        for (int j=0; j<d; j++) {
+            
+            glColor3f((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+            glVertex3f(x, 0, z+diff);
+            glVertex3f(x, 0, z);
+            glVertex3f(x-diff, 0, z+diff);
+            
+            glVertex3f(x, 0, z);
+            glVertex3f(x-diff, 0, z);
+            glVertex3f(x-diff, 0, z+diff);
+
+            x -= diff;
+            xc += 0.01;
+            yc += 0.01;
+            zc += 0.01;
+        }
+        x = l/2;
+        z += diff;
+    }
+    glEnd();
+}
+
+
+
+void renderScene(void) {
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+	gluLookAt(5.0,5.0,5.0, 
+		      0.0,0.0,0.0,
+			  0.0f,1.0f,0.0f);
+
+    drawPlane(9, 3);
+
+    glBegin(GL_LINES);
+	// X axis in red
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-100.0f, 0.0f, 0.0f);
+	glVertex3f(100.0f,0.0f, 0.0f);
+
+	// Y axis in green
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, -100.0f, 0.0f);
+	glVertex3f(0.0f, 100.0f, 0.0f);
+
+	// Z axis in blue
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, -100.0f);
+	glVertex3f(0.0f, 0.0f, 100.0f);
+    
+    glEnd();
+
+
+	// End of frame
+	glutSwapBuffers();
+}
+
+
+int main(int argc, char **argv) {
+
+// init GLUT and the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(800,800);
+	glutCreateWindow("CG@DI-UM");
+		
+// Required callback registry 
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+
+	
+//  OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	
+// enter GLUT's main cycle
+	glutMainLoop();
+	
+	return 1;
+}
