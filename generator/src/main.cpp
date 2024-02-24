c173c2a9a1b13e155662d750ea984394836ee531
Bruno Gião <a96544@alunos.uminho.pt>
2024-02-24 12:03:33 +0000

    Maybe?
    do check the computations being made, it seems to work for even numbers
    but it is flimsy

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,43 +1,53 @@
 /* Output a 3d model file*/
-#include <cstddef>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <float.h>
 
 struct pair {
 	float x,z;
 };
 
-int gen_plane(float full_size, int divs, char* file)
+int32_t gen_plane(float full_size, int32_t divs, char* file)
 {
 	FILE* output = fopen(file, "w+"); 
 	char buff[512];
 	size_t b_read;
 
 	struct pair curr, tmp;
-	int err = 0, i,j,
-	    tot = (divs+1)*(divs+1);
+	int32_t err = 0, i,j;
+	    /*tot = (divs+1)*(divs+1);*/
 	float div_len = full_size / divs,
-	      off = !(divs % 2) ? 0 : div_len/2,
+	      off = !(divs % 2) ? 0 : div_len/divs,
 	      x,z;
 
 	for (i=0; i < divs; i++) {
 		for (j=0; j < divs; j++) {
+			//curr.x = x = i * div_len - div_len + off;
+			curr.x = x = i * div_len + off;
+			curr.z = z = j * div_len - div_len + off;
 			b_read = sprintf(buff, "%.3f 0.000 %.3f\n", curr.x, curr.z);
-			fwrite(buff, sizeof (char),b_read, output);
+			fwrite(buff, sizeof (int8_t),b_read, output);
 
+			curr.x = tmp.x = x;
+			curr.z = tmp.z = z + div_len;
 			b_read = sprintf(buff, "%.3f 0.000 %.3f\n", curr.x, curr.z);
-			fwrite(buff, sizeof (char),b_read, output);
+			fwrite(buff, sizeof (int8_t),b_read, output);
 			
+			curr.x = x - div_len;
+			curr.z = z;
 			b_read = sprintf(buff, "%.3f 0.000 %.3f\n", curr.x, curr.z);
-			fwrite(buff, sizeof (char),b_read, output);
-			fwrite(buff, sizeof (char),b_read, output);
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			fwrite(buff, sizeof (int8_t),b_read, output);
 
 			b_read = sprintf(buff, "%.3f 0.000 %.3f\n", tmp.x, tmp.z);
-			fwrite(buff, sizeof (char),b_read, output);
+			fwrite(buff, sizeof (int8_t),b_read, output);
 
+			curr.x = tmp.x - div_len;
+			curr.z = tmp.z;
 			b_read = sprintf(buff, "%.3f 0.000 %.3f\n\n", curr.x, curr.z);
-			fwrite(buff, sizeof (char),b_read, output);
+			fwrite(buff, sizeof (int8_t),b_read, output);
 		}
 	}
 
@@ -45,9 +55,9 @@ int gen_plane(float full_size, int divs, char* file)
 	return err;
 }
 
-int main(int argc, char**argv)
+int32_t main(int32_t argc, char**argv)
 {
-	int err = 0;
+	int32_t err = 0;
 	if (argc < 2) {
 		err = -1;
 		goto clean;
