22034cdc8b00c6be152a912c51045736391c8a77
Cookies <79994194+Tsushiko@users.noreply.github.com>
2024-02-25 22:01:09 +0000

    Esfera

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -47,6 +47,47 @@ int32_t gen_plane(float full_size, int32_t divs, char* file)
 	fclose(output);
 	return err;
 }
+void drawSphere(float radius, int slices, int stacks) {
+	float px, py, pz;
+	float alfa_diff = 2 * M_PI / slices;
+	float beta_diff = M_PI / stacks;
+	float alfa = 0;
+	float beta = 0;
+	glPolygonMode(GL_FRONT, GL_LINE);
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < slices; i++) {
+		for (int j = 0; j < stacks; j++) {
+			px = radius * cos(beta - M_PI_2) * cos(alfa);
+			py = radius * sin(beta - M_PI_2);
+			pz = radius * cos(beta - M_PI_2) * sin(alfa);
+			glVertex3f(px, py, pz);
+			px = radius * cos(beta - M_PI_2 + beta_diff) * cos(alfa + alfa_diff);
+			py = radius * sin(beta - M_PI_2 + beta_diff);
+			pz = radius * cos(beta - M_PI_2 + beta_diff) * sin(alfa + alfa_diff);
+			glVertex3f(px, py, pz);
+			px = radius * cos(beta - M_PI_2) * cos(alfa + alfa_diff);
+			py = radius * sin(beta - M_PI_2);
+			pz = radius * cos(beta - M_PI_2) * sin(alfa + alfa_diff);
+			glVertex3f(px, py, pz);
+			px = radius * cos(beta - M_PI_2) * cos(alfa);
+			py = radius * sin(beta - M_PI_2);
+			pz = radius * cos(beta - M_PI_2) * sin(alfa);
+			glVertex3f(px, py, pz);
+			px = radius * cos(beta - M_PI_2 + beta_diff) * cos(alfa);
+			py = radius * sin(beta - M_PI_2 + beta_diff);
+			pz = radius * cos(beta - M_PI_2 + beta_diff) * sin(alfa);
+			glVertex3f(px, py, pz);
+			px = radius * cos(beta - M_PI_2 + beta_diff) * cos(alfa + alfa_diff);
+			py = radius * sin(beta - M_PI_2 + beta_diff);
+			pz = radius * cos(beta - M_PI_2 + beta_diff) * sin(alfa + alfa_diff);
+			glVertex3f(px, py, pz);
+			beta += beta_diff;
+		}
+		beta = 0;
+		alfa += alfa_diff;
+	}
+	glEnd();
+}
 
 int32_t main(int32_t argc, char**argv)
 {
