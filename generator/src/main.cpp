63e3c9ff8baf82e1f9fe206c8fec553552ab22cb
Bruno Gião <a96544@alunos.uminho.pt>
2024-02-29 15:41:39 +0000

    gen oblig prims

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -1,15 +1,134 @@
 /* Output a 3d model file*/
-#include <bits/types/FILE.h>
 #include <cstddef>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <float.h>
+#include <math.h>
 
 struct pair {
 	float x,z;
 };
+
+int32_t gen_sphere(float radius, int32_t slices, int32_t stacks, char*file)
+{
+	FILE* output = fopen(file, "w+");
+	char buff[512];
+	float px, py, pz, alpha_diff = 2 * M_PI / slices,
+	      beta_diff = M_PI / stacks, alpha = 0, beta = 0;
+	size_t b_read;
+	for (int i = 0; i < slices; i++) {
+		for (int j = 0; j < stacks; j++) {
+			b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n",
+					  radius * cos(beta - M_PI_2) * cos(alpha),
+					  radius * sin(beta - M_PI_2),
+					  radius*cos(beta+M_PI_2)*sin(alpha));
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n",
+					  radius * cos(beta - M_PI_2 + beta_diff)
+					  	* cos(alpha + alpha_diff),
+					  radius * sin(beta - M_PI_2 + beta_diff),
+					  radius*cos(beta+M_PI_2+beta_diff)*
+					  	sin(alpha + alpha_diff));
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n",
+					  radius * cos(beta - M_PI_2)
+					  	* cos(alpha + alpha_diff),
+					  radius * sin(beta - M_PI_2),
+					  radius*cos(beta+M_PI_2)
+					  	*sin(alpha + alpha_diff));
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n",
+					  radius * cos(beta - M_PI_2) * cos(alpha),
+					  radius * sin(beta - M_PI_2),
+					  radius*cos(beta+M_PI_2)*sin(alpha));
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n",
+					  radius * cos(beta - M_PI_2 + beta_diff) * cos(alpha),
+					  radius * sin(beta - M_PI_2 + beta_diff),
+					  radius*cos(beta+M_PI_2 + beta_diff)*sin(alpha));
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n",
+					  radius * cos(beta - M_PI_2 + beta_diff)
+					  	* cos(alpha + alpha_diff),
+					  radius * sin(beta - M_PI_2 + beta_diff),
+					  radius*cos(beta+M_PI_2+beta_diff)*
+					  	sin(alpha + alpha_diff));
+			fwrite(buff, sizeof (int8_t),b_read, output);
+			beta += beta_diff;
+		}
+		beta = 0;
+		alpha += alpha_diff;
+	}
+	fclose(output);
+	return 0;
+}
+
+int32_t gen_cone(float radius, float height, int32_t slices, int32_t stacks, char*file)
+{
+	FILE* output = fopen(file, "w+");
+	char buff[512];
+	size_t b_read;
+	int i, j, r = 0;
+	float y=0;
+	float angle = 0;
+	float cur_rad = radius;
+	float angle_diff = 2*M_PI/slices;
+	float xz_diff = radius/stacks;
+	float y_diff = height/stacks;
+    
+	for(i=0; i<stacks; i++) {
+		for(j=0; j<slices; j++) {
+            // Bottom face
+			if (i==0) {
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", cur_rad*sin(angle+angle_diff), 0.f, cur_rad*cos(angle+angle_diff));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", 0.f, 0.f, 0.f);
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", cur_rad*sin(angle), 0.f, cur_rad*cos(angle));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+			}
+           
+			if (i==stacks-1){              
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", cur_rad*sin(angle+angle_diff), y, cur_rad*cos(angle+angle_diff));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", 0.f, height, 0.f);
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n\n", cur_rad*sin(angle), y, cur_rad*cos(angle));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+			}
+			else {
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", (cur_rad-xz_diff)*sin(angle+angle_diff), y+y_diff,
+						  (cur_rad-xz_diff)*cos(angle+angle_diff));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", (cur_rad-xz_diff)*sin(angle), y+y_diff,
+						  (cur_rad-xz_diff)*cos(angle));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", cur_rad*sin(angle+angle_diff), y,
+						  cur_rad*cos(angle+angle_diff));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", (cur_rad-xz_diff)*sin(angle), y+y_diff,
+						  (cur_rad-xz_diff)*cos(angle));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n", cur_rad*sin(angle), y,
+						  cur_rad*cos(angle));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+				b_read = snprintf(buff, 512, "%.3f %.3f %.3f\n\n", cur_rad*sin(angle+angle_diff), y,
+						  cur_rad*cos(angle+angle_diff));
+				fwrite(buff, sizeof (int8_t),b_read, output);
+			}
+			angle += angle_diff;
+		}
+		angle = 0;
+		y += y_diff;
+		cur_rad -= xz_diff;
+	}
+	fclose(output);
+	return r;
+}
+
 
 int32_t gen_box(float l, int32_t d, char* file)
 {
@@ -204,7 +323,7 @@ int32_t main(int32_t argc, char**argv)
 				atoi(argv[3]),
 				argv[4]);
 	}
-	if (!strcmp(argv[1], "box")) {
+	else if (!strcmp(argv[1], "box")) {
 		if (argc != 5) {
 			err = -1;
 			goto clean;
@@ -213,7 +332,34 @@ int32_t main(int32_t argc, char**argv)
 			      atoi(argv[3]),
 			      argv[4]);
 	}
-
+	else if (!strcmp(argv[1], "cone")) {
+		if (argc != 7) {
+			err = -1;
+			goto clean;
+		}
+		err = gen_cone(atof(argv[2]),
+				atof(argv[3]),
+				atoi(argv[4]),
+				atoi(argv[5]),
+				argv[6]);
+	}
+	else if (!strcmp(argv[1], "sphere")) {
+		if (argc != 6) {
+			err = -1;
+			goto clean;
+		}
+		err = gen_sphere(atof(argv[2]),
+			      atoi(argv[3]),
+			      atoi(argv[4]),
+			      argv[5]);
+	}
+	else if (!strcmp(argv[1], "torus")) {
+		if (argc != 5) {
+			err = -1;
+			goto clean;
+		}
+		err = -1;
+	}
 
 clean:
 	return err;
