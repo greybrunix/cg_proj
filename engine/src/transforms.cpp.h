6deeb9b2dde5525ab7e52e17cba042eb727ba9ea
Bruno Gião <a96544@alunos.uminho.pt>
2024-03-26 21:37:33 +0000

    class header

---
+++ engine/src/transforms.cpp.h
@@ -0,0 +1,27 @@
+/* Classes */
+class transform {
+	private:	
+	
+	public: 
+
+};
+
+class rotate {
+	private:	
+	
+	public: 
+
+};
+
+class scale {
+	private:
+
+	public:
+
+};
+
+class translate {
+	private:
+
+	public:
+};
